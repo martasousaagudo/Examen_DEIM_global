f7c02df3061cd9a58e422fe054dabbfdbae2e3c7
martasousaagudo <marta.sousa.agudo@gmail.com>
2021-03-10 19:58:20 +0100

    texturas aplicadas+ script colisiones de los huevos

---
+++ Sousa_Marta/Assets/SCRIPTS/colision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colision : MonoBehaviour
+{
+    Instanciador instanciador;
+    public int resta;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instanciador = GameObject.Find("Instanciador").GetComponent<Instanciador>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Egg_FBX")
+        {
+            resta++;
+            Destroy(gameObject);
+        }
+    }
+}
