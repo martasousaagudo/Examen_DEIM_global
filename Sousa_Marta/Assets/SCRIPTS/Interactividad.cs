e531b0526830760d9a024e643b3a86d8c1cba608
martasousaagudo <marta.sousa.agudo@gmail.com>
2021-03-10 19:24:24 +0100

    movimiento de camara y de la caja

---
+++ Sousa_Marta/Assets/SCRIPTS/Interactividad.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactividad : MonoBehaviour
+{
+
+    public float velocidad = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverCaja();
+    }
+
+    public void MoverCaja()
+    {
+        float desplx = Input.GetAxis("Horizontal");
+        float desply = Input.GetAxis("Vertical");
+
+        //moverse a la velocidad establecida
+        transform.Translate(Vector3.right * Time.deltaTime * velocidad * desplx);
+        transform.Translate(Vector3.forward * Time.deltaTime * velocidad * desply);
+
+
+
+
+
+    }
+
+
+
+}
---
+++ Sousa_Marta/Assets/SCRIPTS/MovCamara.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovCamara : MonoBehaviour
+{
+    public GameObject caja;
+    [SerializeField] Transform cubo;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(cubo.position.x, transform.position.y, transform.position.z);
+        
+
+    }
+}
