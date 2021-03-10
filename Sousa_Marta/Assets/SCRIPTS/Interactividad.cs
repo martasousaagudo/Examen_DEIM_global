a76704bff2e7460c32bd4d2c2a24c57986ee96c4
martasousaagudo <marta.sousa.agudo@gmail.com>
2021-03-10 20:30:36 +0100

    intento de hacer los limites

--- Sousa_Marta/Assets/SCRIPTS/Interactividad.cs
+++ Sousa_Marta/Assets/SCRIPTS/Interactividad.cs
@@ -7,6 +7,9 @@ public class Interactividad : MonoBehaviour
     public GameObject cesta;
   
     public float velocidad = 3f;
+
+    private bool inMarginMoveX = true;
+    private bool inMarginMoveZ = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,24 +24,79 @@ public class Interactividad : MonoBehaviour
 
     public void MoverCaja()
     {
-        float desplx = Input.GetAxis("Horizontal");
-        float desply = Input.GetAxis("Vertical");
+        float desplX = Input.GetAxis("Horizontal");
+        float desplZ = Input.GetAxis("Vertical");
 
         //moverse a la velocidad establecida
-        transform.Translate(Vector3.right * Time.deltaTime * velocidad * desplx);
-        transform.Translate(Vector3.forward * Time.deltaTime * velocidad * desply);
+       // transform.Translate(Vector3.right * Time.deltaTime * velocidad * desplX);
+        //transform.Translate(Vector3.forward * Time.deltaTime * velocidad * desplZ);
 
 
+        float myPosX = transform.position.x;
+        float myPosZ = transform.position.z;
 
-
+        if (inMarginMoveX)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * velocidad * desplX);
+        }
+        if (inMarginMoveZ)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * velocidad * desplZ);
+        }
 
     }
+        void checkRestrX(float myPosX, float desplX)
+        {
+            if (myPosX > -19 && desplX < 0)
+            {
+                inMarginMoveX = false;
+            }
+            else if (myPosX < 19 && desplX > 0)
+            {
+                inMarginMoveX = true;
+            }
+            else if (myPosX > -19 && desplX > 0)
+            {
+                inMarginMoveX = false;
+            }
+            else if (myPosX > 19 && desplX < 0)
+            {
+                inMarginMoveX = true;
+            }
+        }
+
+        void checkRestrZ(float myPosZ, float desplZ)
+        {
+            //Retricción en Y
+            if (myPosZ < -19 && desplZ < 0)
+            {
+                inMarginMoveZ = false;
+            }
+            else if (myPosZ < 19 && desplZ > 0)
+            {
+                inMarginMoveZ = true;
+            }
+            else if (myPosZ > -19 && desplZ > 0)
+            {
+                inMarginMoveZ = false;
+            }
+            else if (myPosZ > 19 && desplZ < 0)
+            {
+                inMarginMoveZ = true;
+            }
+        }
+    
+
 
     public void Cesta()
     {
         if (Input.GetKey(KeyCode.C))
         {
             cesta.SetActive(true);
+        }
+        else
+        {
+            cesta.SetActive(false);
         }
     }
 
---
+++ Sousa_Marta/Assets/SCRIPTS/MovHuevo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovHuevo : MonoBehaviour
+{
+    private float velocidadHuevo;
+    public GameObject Cube;
+    Interactividad movimientoHuevo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cube = GameObject.Find("Egg_FBX");
+        movimientoHuevo = Cube.GetComponent<Interactividad>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float PosY = transform.position.y;
+        if(PosY < -2)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
